fce7249b5119078badbd3d7754986b865ecd433e
Farhad Khurami <fkhurami98@gmail.com>
2023-07-30 22:46:26 +0100

    tidy up

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
+import time
 from bs4 import BeautifulSoup
-from playwright.sync_api import sync_playwright
-from playwright.sync_api import TimeoutError
 from pprint import pprint
 import random
 import json
+from playwright.sync_api import sync_playwright, TimeoutError
 
 
 def get_reddit_page(url):
@@ -11,7 +11,7 @@ def get_reddit_page(url):
     Visits a reddit page using playwright and extracts the html.
 
     Args:
-        url(str): The url of the reddit page for scraping.
+        url (str): The url of the reddit page for scraping.
 
     Returns:
         str: A string containing the html of the reddit page.
@@ -26,7 +26,7 @@ def get_reddit_page(url):
         try:
             accept_button = page.wait_for_selector(
                 "shreddit-interactable-element#accept-all-cookies-button button",
-                timeout=30000,
+                timeout=20*1000,
             )
             accept_button.click()
         except TimeoutError:
@@ -67,7 +67,7 @@ def parse_reddit_html(html):
     Parses the HTML of a Reddit page using BeautifulSoup and extracts post elements.
 
     Args:
-        html(str): The HTML content of the Reddit page.
+        html (str): The HTML content of the Reddit page.
 
     Returns:
         list: A list of BeautifulSoup elements representing post elements.
@@ -82,7 +82,7 @@ def extract_post_metadata(post_element):
     Extracts post information from a BeautifulSoup element representing a post.
 
     Args:
-        post_element(bs4.element.Tag): BeautifulSoup element representing a post.
+        post_element (bs4.element.Tag): BeautifulSoup element representing a post.
 
     Returns:
         dict: A dictionary containing post information.
@@ -119,30 +119,74 @@ def extract_post_metadata(post_element):
     return post_data
 
 
-if __name__ == '__main__':
+def extract_individual_post_info(html_code):
+    """
+    Extracts individual post information from the HTML content.
+
+    Args:
+        html_code (str): The HTML code of an individual post.
+
+    Returns:
+        dict: A dictionary containing post information.
+    """
+    soup = BeautifulSoup(html_code, "html.parser")
+
+    parent_div = soup.select_one("div.mb-sm.mb-xs.px-md.xs\\:px-0")
+
+    if not parent_div:
+        post_info = {
+            "Opinions": "Post content not found. Likely to be blank",
+        }
+    else:
+        post_text = parent_div.find_all("p")
+        opinions = [paragraph.text.strip() for paragraph in post_text]
+        post_info = {
+            "Opinions": opinions,
+        }
+
+    return post_info
+
+
+if __name__ == "__main__":
     # Call the function to visit a reddit page
     html = get_reddit_page(url="https://www.reddit.com")
-    
+
     # Parse the larger HTML page using BeautifulSoup
     post_elements = parse_reddit_html(html)
-    
+
     # List to store the dictionaries of extracted post information
     post_data_list = []
-    
+
     # Loop through all the post elements found
     for post_element in post_elements:
         # Extract post information
         post_data = extract_post_metadata(post_element)
-    
+
         # Append the dictionary to the list
         post_data_list.append(post_data)
-    
-    # Print the extracted information for each post
+
+    # Read post_data_list from the JSON file (if it exists)
+    try:
+        with open("posts_metadata.json", "r") as file:
+            existing_post_data_list = json.load(file)
+    except FileNotFoundError:
+        existing_post_data_list = []
+
+    # Append extracted individual post info to the post_data_list
     for post_data in post_data_list:
-        print("--------------")
-        for key, value in post_data.items():
-            print(f"{key}: {value}")
-    
-    # Write post_data_list to a JSON file
+        permalink = post_data["Permalink"]
+        print(f"Extracting HTML from {permalink}")
+        html_content = get_reddit_page(permalink)
+        data = extract_individual_post_info(html_content)
+        post_data.update(data)  # Append individual post info to the existing data
+        pprint(post_data)
+        time.sleep(30)
+
+    # Combine the existing and new post_data_lists
+    combined_post_data_list = existing_post_data_list + post_data_list
+
+    # Write the combined post_data_list to the JSON file
     with open("posts_metadata.json", "w") as json_file:
-        json.dump(post_data_list, json_file, indent=4)
+        json.dump(combined_post_data_list, json_file, indent=4)
+
+
