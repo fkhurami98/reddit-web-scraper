c3fbc41f64ac41eb5d21f27188b30d73c13ea235
Farhad Khurami <fkhurami98@gmail.com>
2023-07-30 20:35:17 +0100

    Finally something fucking works

--- main.py
+++ main.py
@@ -24,10 +24,10 @@ def get_reddit_homepage():
 
         return html
 
-
-
+# Call the function to get the Reddit homepage
 html = get_reddit_homepage()
 
+# Write the HTML to a file
 with open('homepage.html', 'w') as f:
     f.write(html)
 
@@ -35,34 +35,28 @@ with open('homepage.html', 'w') as f:
 soup = BeautifulSoup(html, 'html.parser')
 
 # Find all elements with the shreddit-post class (the provided HTML snippet)
-post_elements = soup.find_all('shreddit-post')
+post_elements = soup.select('shreddit-post')
 
 # Loop through all the post elements found
 for post_element in post_elements:
-    # Convert the element back to a string representation
-    extracted_html_snippet = str(post_element)
-
-    soup_post = BeautifulSoup(extracted_html_snippet, 'html.parser')
-
     # Extracting post details
-    post_title_elem = soup_post.find('h3', {'class': '_eYtD2XCVieq6emjKBH3m'})
+    post_title_elem = post_element.select_one('[slot="title"]')
     post_title = post_title_elem.text.strip() if post_title_elem else "N/A"
 
-    # Extracting permalink to full post
-    permalink_elem = soup_post.find('a', {'class': 'SQnoC3ObvgnGjWt90zD9Z'})
-    permalink = "https://www.reddit.com" + permalink_elem['href'] if permalink_elem else "N/A"
-    subreddit = permalink.split('/')[4] if permalink != "N/A" else "N/A"
+    permalink_elem = post_element.get('permalink')
+    permalink = "https://www.reddit.com" + permalink_elem if permalink_elem else "N/A"
 
-    # Extracting post author
-    author_elem = soup_post.find('a', class_='_2tbHP6ZydRpjI44J3syuqC _23wugcdiaj44hdfugIAlnX oQctV4n0yUb0uiHDdGnmE')
-    author = author_elem.text if author_elem else "N/A"
+    subreddit_elem = post_element.get('subreddit-prefixed-name')
+    subreddit = subreddit_elem if subreddit_elem else "N/A"
 
-    # Extracting the number of comments
-    comment_count_elem = soup_post.find('span', {'class': 'FHCV02u6Cp2zYL0fhQPsO'})
-    comment_count = int(comment_count_elem.text.split()[0]) if comment_count_elem else 0
+    author_elem = post_element.get('author')
+    author = author_elem if author_elem else "N/A"
 
-    post_score_elem = soup_post.find('div', {'class': '_1rZYMD_4xY3gRcSS3p8ODO'})
-    post_score = int(post_score_elem.text) if post_score_elem else 0
+    comment_count_elem = post_element.get('comment-count')
+    comment_count = int(comment_count_elem) if comment_count_elem else 0
+
+    post_score_elem = post_element.get('score')
+    post_score = int(post_score_elem) if post_score_elem else 0
 
     # Printing the extracted information for each post
     print("--------------")
