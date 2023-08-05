30f5e7fc02580df6101a0ae0a3f451f9bcbf8f2e
Farhad Khurami <fkhurami98@gmail.com>
2023-08-05 13:27:01 +0100

    Added code to insert post data into postgres database + refactoring

--- database/db.py
+++ database/db.py
@@ -1,31 +1,79 @@
+import os
+import json
 from sqlalchemy import create_engine, Column, Integer, String, Text, TIMESTAMP
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import inspect  
 
-# Replace 'your_username', 'your_password', 'your_host', 'your_database' with actual PostgreSQL details
-DATABASE_URL = "postgresql://postgres:password@localhost:5432/reddit_scraper_1"
-engine = create_engine(DATABASE_URL)
-Base = declarative_base()
+def insert_to_db(database_url, json_folder_path):
+    """
+    Main function to create table if not exists and insert data from JSON files.
 
-class Post(Base):
-    __tablename__ = 'posts'
+    Args:
+        database_url: Database connection URL.
+        json_folder_path: Path to the folder containing JSON files.
+    """
+    # Create the engine
+    engine = create_engine(database_url)
     
-    id = Column(Integer, primary_key=True)
-    post_title = Column(String(255))
-    permalink = Column(String(255))
-    subreddit = Column(String(100))
-    author = Column(String(100))
-    num_comments = Column(Integer)
-    post_score = Column(Integer)
-    post_content = Column(Text)
-    time_stamp = Column(TIMESTAMP)
+    # Declarative base for ORM
+    Base = declarative_base()
 
-# Create the table in the database
-Base.metadata.create_all(engine)
+    # Define the Post class
+    class Post(Base):
+        __tablename__ = "posts"
 
-# Create a session
-Session = sessionmaker(bind=engine)
-session = Session()
+        id = Column(Integer, primary_key=True)
+        post_title = Column(Text)
+        permalink = Column(Text, unique=True)
+        subreddit = Column(String(100))
+        author = Column(String(100))
+        num_comments = Column(Integer)
+        post_score = Column(Integer)
+        post_content = Column(Text)
+        time_stamp = Column(TIMESTAMP)
+    
+    # Session creation
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-# Now you can use the session to add, query, and manipulate Post objects
+    # Check if the table exists, create it if not
+    inspector = inspect(engine)  # Create an inspector for the engine
+    if not inspector.has_table("posts"):  # Use the inspector to check table existence
+        Base.metadata.create_all(engine)
 
+    # Iterate through JSON files
+    for json_file_name in os.listdir(json_folder_path):
+        json_file_path = os.path.join(json_folder_path, json_file_name)
+
+        with open(json_file_path, "r") as json_file:
+            data = json.load(json_file)
+
+        # Insert data into the database
+        for post_data in data:
+            permalink = post_data.get("Permalink")
+            existing_post = session.query(Post).filter_by(permalink=permalink).first()
+
+            if existing_post is None:
+                post = Post(
+                    post_title=post_data.get("Post Title"),
+                    permalink=permalink,
+                    subreddit=post_data.get("Subreddit"),
+                    author=post_data.get("Author"),
+                    num_comments=post_data.get("Number of Comments"),
+                    post_score=post_data.get("Post Score"),
+                    post_content=post_data.get("Post Content"),
+                    time_stamp=post_data.get("Time Stamp"),
+                )
+                session.add(post)
+
+        # Commit changes after processing each JSON file
+        session.commit()
+
+    # Close the session
+    session.close()
+
+if __name__ == '__main__':
+    DATABASE_URL = "postgresql://postgres:password@localhost:5432/reddit_scraper_1"
+    JSON_FOLDER_PATH = "subreddit_page_data"
+    insert_to_db(DATABASE_URL, JSON_FOLDER_PATH)
