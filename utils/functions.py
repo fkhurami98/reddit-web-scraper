781ad812459346d2d055c1ec065b5e0b1c019a2e
Farhad Khurami <fkhurami98@gmail.com>
2023-10-08 13:38:54 +0100

    Added class to functions.py to get current path for json_data_folder

--- utils/functions.py
+++ utils/functions.py
@@ -1,1 +1,10 @@
-# Function for getting file path where the function is executed
+import os
+
+
+class CurrentPath:
+    def __init__(self, caller_file):
+        self.current_directory = os.path.dirname(os.path.abspath(caller_file))
+        self.save_path = os.path.join(self.current_directory, "json_data_folder")
+
+    def get_save_path(self):
+        return self.save_path
