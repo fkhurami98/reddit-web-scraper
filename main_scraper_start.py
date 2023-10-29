a6d16c74020584b522a0fe1a16be6d67813c8e42
Farhad Khurami <fkhurami98@gmail.com>
2023-10-29 20:27:24 +0000

    Moved some code to functions

--- main_scraper_start.py
+++ main_scraper_start.py
@@ -13,16 +13,16 @@ Dependencies:
 - db_handler: Contains methods for interacting with the database.
 - utils: Consists of utility functions and constants used in scraping and data processing.
 - scraping_helpers: Provides specific methods for scraping Reddit and processing data.
-
+- distributed_helpers: (WIP) Provides functionality to run scrapers on distributed nodes.
 Usage:
 Run the script directly to initiate the scraping process: `python3 main_reddit_scraper.py`
 """
 
 import os
-from fake_useragent import UserAgent
+import sys
 from db_handler.db import insert_to_db
-from utils.constants import URL_LIST, DATABASE_URL_HOME, DATABASE_URL_VPN
-from utils.functions import get_save_path
+from utils.constants import URL_LIST
+from utils.functions import get_save_path,  get_ip_addresses, setup_data_folder, determine_access_path
 from scraping_helpers.fetch_post_details import process_all_json_files_in_folder
 from scraping_helpers.fetch_subreddit_metadata import (
     scrape_multiple_subreddits_concurrently,
@@ -32,10 +32,16 @@ from scraping_helpers.fetch_subreddit_metadata import (
 
 if __name__ == "__main__":
     JSON_SAVE_PATH = get_save_path(__file__)
+    
+    current_tailscale_ip = get_ip_addresses()["tailscale_ipv4"]
+    print(current_tailscale_ip)
+    
+    # Determine the database URL based on the machine's IP
+    database_url = determine_access_path(current_tailscale_ip)
 
+    print(database_url)
     # Creates /json_data_folder folder if not exists
-    if not os.path.exists("json_data_folder"):
-        os.makedirs("json_data_folder")
+    setup_data_folder('json_data_folder')
 
     # Clean up JSON files before initiating scraping
     clear_json_files_in_folder(folder_path=JSON_SAVE_PATH)
@@ -47,4 +53,4 @@ if __name__ == "__main__":
     process_all_json_files_in_folder(folder_path=JSON_SAVE_PATH)
 
     # Reads JSON files from /json_data_folder and sends to db
-    insert_to_db(database_url=DATABASE_URL_HOME, json_folder_path=JSON_SAVE_PATH)
+    insert_to_db(database_url="postgresql://postgres:password@localhost:5432/reddit_scraper_1", json_folder_path=JSON_SAVE_PATH)
