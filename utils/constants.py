ea06339c6e9da9851a3e214e95c987a9ce67b174
Farhad Khurami <fkhurami98@gmail.com>
2023-10-29 20:24:57 +0000

    Changed URL access points to a dict

--- utils/constants.py
+++ utils/constants.py
@@ -1,11 +1,17 @@
 
 
-DATABASE_URL_VPN: str = "postgresql://postgres:password@100.68.124.90:5432/reddit_scraper_1"
-DATABASE_URL_HOME: str = "postgresql://postgres:password@192.168.1.129:5432/reddit_scraper_1"
+DATABASE_URLS = {
+    'TAILSCALE': "postgresql://postgres:password@100.68.124.90:5432/reddit_scraper_1",
+    'LOCAL': "postgresql://postgres:password@192.168.1.129:5432/reddit_scraper_1",
+    'LOCALHOST': "postgresql://postgres:password@localhost:5432/reddit_scraper_1",
+    # ... Add more access paths as needed
+}
 
 MAX_ATTEMPTS: int = 2
 WAIT_BETWEEN_ATTEMPTS: int = 3
 CONCURRENCEY_LIMIT: int = 6
+
+
 
 
 
